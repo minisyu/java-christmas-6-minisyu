b688ba9503268854d2b9cd77b79cf973c77b4719
minisyu <iswater.kim@gmail.com>
2023-11-10 17:39:23 +0900

    feat: Reservation에 정적 팩토리 메소드 추가

--- src/main/java/christmas/domain/Reservation.java
+++ src/main/java/christmas/domain/Reservation.java
@@ -4,8 +4,12 @@ public class Reservation {
     private final MenuItems menuItems;
     private final int visitDate;
 
-    public Reservation(MenuItems menuItems, int visitDate) {
+    private Reservation(MenuItems menuItems, int visitDate) {
         this.menuItems = menuItems;
         this.visitDate = visitDate;
+    }
+
+    public static Reservation from(MenuItems menuItems, int visitDate) {
+        return new Reservation(menuItems, visitDate);
     }
 }

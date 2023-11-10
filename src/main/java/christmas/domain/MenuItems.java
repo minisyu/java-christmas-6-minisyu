e57a398e32c2d57590b8afefc66645a7daf32274
minisyu <iswater.kim@gmail.com>
2023-11-10 15:54:35 +0900

    feat: MenuItem의 일급 컬렉션 MenuItems 생성

---
+++ src/main/java/christmas/domain/MenuItems.java
@@ -0,0 +1,11 @@
+package christmas.domain;
+
+import java.util.List;
+
+public class MenuItems {
+    private final List<MenuItem> menuItems;
+
+    public MenuItems(List<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
+}

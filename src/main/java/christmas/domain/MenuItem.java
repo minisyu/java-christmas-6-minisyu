e0980496eb23813eb97640caab681cb3a95fc43b
minisyu <iswater.kim@gmail.com>
2023-11-10 17:31:29 +0900

    refactor: MenuItem에 기능 추가
    - 메뉴의 개수가 1개 이상인지 검증하는 로직 추가
    - getter 추가

--- src/main/java/christmas/domain/MenuItem.java
+++ src/main/java/christmas/domain/MenuItem.java
@@ -4,8 +4,30 @@ public class MenuItem {
     private final Menu menu;
     private final int quantity;
 
-    public MenuItem(Menu menu, int quantity) {
+    private MenuItem(Menu menu, int quantity) {
+        validateQuantity(quantity);
         this.menu = menu;
         this.quantity = quantity;
+    }
+
+    public static MenuItem from(Menu menu, int quantity) {
+        return new MenuItem(menu, quantity);
+    }
+
+    /**
+     * 메뉴의 개수를 1이상의 숫자를 입력하는지 검증
+     */
+    private static void validateQuantity(int quantity) {
+        if (quantity < 1) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public int getQuantity() {
+        return quantity;
     }
 }

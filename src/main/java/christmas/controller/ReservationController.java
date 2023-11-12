b669e882f063f44bdf5447786835961d0bf1d5e3
minisyu <iswater.kim@gmail.com>
2023-11-13 01:32:25 +0900

    feat: `ReservationController` 생성
    - 사용자 입력 값 기능 구현

---
+++ src/main/java/christmas/controller/ReservationController.java
@@ -0,0 +1,21 @@
+package christmas.controller;
+
+import christmas.domain.Reservation;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class ReservationController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public ReservationController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        // 사용자가 입력한 메뉴와 개수 및 날짜를 입력 받는다
+        Reservation reservation = inputView.inputReservation();
+
+    }
+}

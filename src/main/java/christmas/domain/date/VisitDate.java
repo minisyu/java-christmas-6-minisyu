95c20f51f2d05da0bda0afcf7c41b3300b00986f
minisyu <iswater.kim@gmail.com>
2023-11-13 02:10:22 +0900

    refactor: `VisitDate`에 기능 추가
    - 할인 혜택 적용 여부에 대한 메소드 추가
    - 크리스마스 디데이 할인액을 계산하는 메소드 추가
    - 할인 혜택과 관련된 상수 추가
    - 날짜를 반환하는 메소드 추가

--- src/main/java/christmas/domain/date/VisitDate.java
+++ src/main/java/christmas/domain/date/VisitDate.java
@@ -1,17 +1,21 @@
 package christmas.domain.date;
 
 import christmas.exception.VisitDateException;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.List;
 
 /**
  * 예약 날짜
  */
 public class VisitDate {
+    public static final int DEFAULT_YEAR = 2023;
+    public static final int DEFAULT_MONTH = 12;
+    public static final List<Integer> specialDays = List.of(3, 10, 17, 24, 25, 31);
+    public static final int INITIAL_CHRISTMAS_PRICE = 1_000;
+    public static final int DAILY_CHRISTMAS_DISCOUNT = 100;
     private static final int MIN_DAY = 1;
     private static final int MAX_DAY = 31;
-    private static final int DEFAULT_YEAR = 2023;
-    private static final int DEFAULT_MONTH = 12;
-    
     private final LocalDate visitDate;
 
     private VisitDate(LocalDate visitDate) {
@@ -34,6 +38,52 @@ public class VisitDate {
         if (dayOfMonth < MIN_DAY || dayOfMonth > MAX_DAY) {
             throw new VisitDateException();
         }
+    }
+
+    /**
+     * @param otherDate 다른 날짜
+     * @return 다른 날짜를 지나지 않았으면 true, 지났으면 false
+     */
+    public boolean isNotAfter(LocalDate otherDate) {
+        return !visitDate.isAfter(otherDate);
+    }
+
+    /**
+     * @return 크리스마스 디데이 할인액
+     */
+    public int calculateChristmasDiscount() {
+        int daysUntilChristmas = visitDate.getDayOfMonth() - 1;
+        return INITIAL_CHRISTMAS_PRICE + (DAILY_CHRISTMAS_DISCOUNT * (daysUntilChristmas));
+    }
+
+    /**
+     * @return 주말이면 true, 아니면 false
+     */
+    public boolean isWeekend() {
+        DayOfWeek dayOfWeek = visitDate.getDayOfWeek();
+        return dayOfWeek == DayOfWeek.FRIDAY || dayOfWeek == DayOfWeek.SATURDAY;
+    }
+
+    /**
+     * @return 평일이면 true, 아니면 false
+     */
+    public boolean isWeekday() {
+        return !isWeekend();
+    }
+
+    /**
+     * @return 별이 있으면 true, 없으면 false
+     */
+    public boolean isSpecialDay() {
+        int dayOfMonth = visitDate.getDayOfMonth();
+        return specialDays.contains(dayOfMonth);
+    }
+
+    /**
+     * @return 날짜 반환
+     */
+    public int getVisitDate() {
+        return visitDate.getDayOfMonth();
     }
 }
 

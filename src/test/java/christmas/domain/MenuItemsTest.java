ae066cb781be884b7269a9acf1dc8029ee8338ff
minisyu <iswater.kim@gmail.com>
2023-11-13 03:19:51 +0900

    refactor: `MenuItemsTest` 테스트 추가
    - 주문한 메뉴들의 총 가격을 계산
    - List<MenuItemDto>를 생성하는지 확인

--- src/test/java/christmas/domain/MenuItemsTest.java
+++ src/test/java/christmas/domain/MenuItemsTest.java
@@ -1,8 +1,12 @@
 package christmas.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import christmas.domain.dto.MenuItemDto;
 import christmas.exception.MenuInputException;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -42,5 +46,54 @@ class MenuItemsTest {
         // then
         assertThatThrownBy(() -> MenuItems.from(input))
                 .isInstanceOf(MenuInputException.class);
+    }
+
+    @DisplayName("주문한 메뉴들의 총 가격을 계산")
+    @Test
+    void should_Calculate_Total_Price_Of_MenuItems() {
+        // given
+        String input = "시저샐러드-1,크리스마스파스타-2,아이스크림-1,레드와인-1";
+        MenuItems menuItems = MenuItems.from(input);
+
+        // when
+        int totalPrice = menuItems.calculateMenuItemsTotalPrice();
+
+        // then
+        int expectedPrice = Menu.CAESAR_SALAD.getFoodPrice() +
+                Menu.CHRISTMAS_PASTA.getFoodPrice() * 2 +
+                Menu.ICE_CREAM.getFoodPrice() +
+                Menu.RED_WINE.getFoodPrice();
+
+        assertEquals(expectedPrice, menuItems.calculateMenuItemsTotalPrice());
+    }
+
+    @DisplayName("List<MenuItemDto>를 생성하는지 확인")
+    @Test
+    void should_Create_MenuItemDtoList() {
+        // given
+        String input = "타파스-1,해산물파스타-2,초코케이크-1,레드와인-1,제로콜라-3";
+        MenuItems menuItems = MenuItems.from(input);
+
+        // when
+        List<MenuItemDto> menuItemsDto = menuItems.toMenuItemsDto();
+
+        // then
+        assertNotNull(menuItemsDto);
+        assertEquals(5, menuItemsDto.size());
+
+        assertEquals("타파스", menuItemsDto.get(0).menuName());
+        assertEquals(1, menuItemsDto.get(0).quantity());
+
+        assertEquals("해산물파스타", menuItemsDto.get(1).menuName());
+        assertEquals(2, menuItemsDto.get(1).quantity());
+
+        assertEquals("초코케이크", menuItemsDto.get(2).menuName());
+        assertEquals(1, menuItemsDto.get(2).quantity());
+
+        assertEquals("레드와인", menuItemsDto.get(3).menuName());
+        assertEquals(1, menuItemsDto.get(3).quantity());
+
+        assertEquals("제로콜라", menuItemsDto.get(4).menuName());
+        assertEquals(3, menuItemsDto.get(4).quantity());
     }
 }

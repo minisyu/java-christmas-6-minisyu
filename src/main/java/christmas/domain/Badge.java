6c50213c68a7e8877773cfac4960511d90dc212c
minisyu <iswater.kim@gmail.com>
2023-11-13 13:04:51 +0900

    feat: `Badge` 생성
    - 배지에 대한 enum 클래스 생성
    - 총혜택 금액에 따라 다른 이벤트 배지를 부여하는 메소드 생성
    - BadgeDto 생성하는 메소드 생성

---
+++ src/main/java/christmas/domain/Badge.java
@@ -0,0 +1,53 @@
+package christmas.domain;
+
+import christmas.domain.dto.BadgeDto;
+
+public enum Badge {
+    NONE("없음", 0),
+    STAR("별", 5_000),
+    TREE("트리", 10_000),
+    SANTA("산타", 20_000);
+
+    private final String badgeName;
+    private final int threshold;
+
+    Badge(String badgeName, int threshold) {
+        this.badgeName = badgeName;
+        this.threshold = threshold;
+    }
+
+    /**
+     * @param totalDiscountPrice 총 혜택 금액
+     * @return 총혜택 금액에 따라 다른 이벤트 배지를 부여
+     * <p>
+     * 5천 원 이상: 별
+     * <p>
+     * 1만 원 이상: 트리
+     * <p>
+     * 2만 원 이상: 산타
+     * <p>
+     * 이벤트 배지가 부여되지 않는 경우, "없음"
+     */
+    public static Badge from(int totalDiscountPrice) {
+        if (totalDiscountPrice >= SANTA.threshold) {
+            return SANTA;
+        }
+
+        if (totalDiscountPrice >= TREE.threshold) {
+            return TREE;
+        }
+
+        if (totalDiscountPrice >= STAR.threshold) {
+            return STAR;
+        }
+
+        return NONE;
+    }
+
+    /**
+     * BadgeDto 생성
+     */
+    public BadgeDto toBadgeDto() {
+        return new BadgeDto(badgeName);
+    }
+}

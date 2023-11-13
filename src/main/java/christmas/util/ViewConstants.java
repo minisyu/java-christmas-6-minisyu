ada2644a2e886c5020e537403d1ef26b0d841410
minisyu <iswater.kim@gmail.com>
2023-11-13 19:01:34 +0900

    feat: `ViewConstants` 생성
    - View에서 사용할 상수 클래스 생성

---
+++ src/main/java/christmas/util/ViewConstants.java
@@ -0,0 +1,18 @@
+package christmas.util;
+
+public class ViewConstants {
+    public static final String START_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    public static final String RESERVATION_MENU_MESSAGE = "주문 메뉴";
+    public static final String TOTAL_RESERVATION_PRICE_MESSAGE = "할인 전 총주문 금액";
+    public static final String GIFT_MESSAGE = "증정 메뉴";
+    public static final String DISCOUNT_MESSAGE = "혜택 내역";
+    public static final String TOTAL_DISCOUNT_MESSAGE = "총혜택 금액";
+    public static final String EXPECTED_PAYMENT_MESSAGE = "할인 후 예상 결제 금액";
+    public static final String BADGE_MESSAGE = "12월 이벤트 배지";
+
+    public static final String MINUS = "-";
+    public static final String WON = "원";
+    public static final String COUNT = "개";
+    public static final String NONE = "없음";
+
+}

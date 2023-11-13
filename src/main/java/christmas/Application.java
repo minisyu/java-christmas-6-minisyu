329e756372a2fc28a8b9f66892020cdb7f0f969e
minisyu <iswater.kim@gmail.com>
2023-11-13 19:02:51 +0900

    feat: `Application` 기능 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,13 @@
 package christmas;
 
+import christmas.controller.ReservationController;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        ReservationController reservationController = new ReservationController(new InputView(), new OutputView());
+        reservationController.run();
     }
 }

f61bc6c30437b344cbb291e99b54a002bab1cd69
minisyu <iswater.kim@gmail.com>
2023-11-14 19:04:57 +0900

    refactor: `OutputView` 수정
    - 이벤트 해택 내역 템플릿을 사용함으로써 로직 전면 수정

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -8,158 +8,147 @@ import christmas.domain.dto.MenuItemDto;
 import christmas.util.ViewConstants;
 import java.text.DecimalFormat;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OutputView {
 
-    public void printEvent(ConfirmedReservation reservationDto) {
-        printEventPreView(reservationDto);
+    public void printReservation(ConfirmedReservation confirmedReservation) {
+        System.out.println(createReservationMessage(confirmedReservation));
+    }
 
-        printReservationMenu(reservationDto);
-
-        printTotalReservationPrice(reservationDto);
-
-        printGiftMenu(reservationDto);
-
-        priintEventBenefits(reservationDto);
-
-        printTotalDiscount(reservationDto);
-
-        printFinalPrice(reservationDto);
-
-        printEventBadge(reservationDto);
+    public String createReservationMessage(ConfirmedReservation confirmedReservation) {
+        return String.format(
+                ViewConstants.RESERVATION_MESSAGE_FRAME,
+                formatReservationDate(confirmedReservation),
+                formatOrderedMenuItems(confirmedReservation),
+                formatTotalMenuItemsPrice(confirmedReservation),
+                formatPromotionalGift(confirmedReservation),
+                formatDiscountEvents(confirmedReservation),
+                formatTotalDiscountPrice(confirmedReservation),
+                formatFinalPrice(confirmedReservation),
+                formatBadge(confirmedReservation)
+        );
     }
 
     /**
-     * @param reservationDto 배지 출력
+     * @param confirmedReservation
+     * @return 날짜 출력 문자열
      */
-    private void printEventBadge(ConfirmedReservation reservationDto) {
-        printHeader(ViewConstants.BADGE_MESSAGE);
-        BadgeDto badge = reservationDto.badge();
-        System.out.println(badge.badgeName());
+    private int formatReservationDate(ConfirmedReservation confirmedReservation) {
+        return confirmedReservation.visitDate();
     }
 
     /**
-     * @param reservationDto 할인 후 예상 결제 금액 출력
+     * @param confirmedReservation
+     * @return 주문 메뉴 출력 문자열
+     * <p>
+     * return """ 티본스테이크 1개 바비큐립 1개 초코케이크 2개 제로콜라 1개 """;
      */
-    private void printFinalPrice(ConfirmedReservation reservationDto) {
-        printHeader(ViewConstants.EXPECTED_PAYMENT_MESSAGE);
-        int expectedPrice = reservationDto.finalPrice();
-        System.out.println(expectedPrice + ViewConstants.WON);
-        printFooter();
+    private String formatOrderedMenuItems(ConfirmedReservation confirmedReservation) {
+        List<MenuItemDto> menuItemsDto = confirmedReservation.menuItems();
+
+        return menuItemsDto.stream()
+                .map(menuItemDto -> menuItemDto.menuName() + ViewConstants.SPACE + menuItemDto.quantity()
+                        + ViewConstants.COUNT)
+                .collect(Collectors.joining(ViewConstants.NEW_LINE));
     }
 
     /**
-     * @param reservationDto 총혜택 금액 출력
+     * @param confirmedReservation
+     * @return 할인 전 총주문 금액 문자열 출력
      */
-    private void printTotalDiscount(ConfirmedReservation reservationDto) {
-        printHeader(ViewConstants.TOTAL_DISCOUNT_MESSAGE);
-        int totalDiscountPrice = reservationDto.totalDiscountPrice();
-        printPrice(totalDiscountPrice);
-        printFooter();
+    private String formatTotalMenuItemsPrice(ConfirmedReservation confirmedReservation) {
+        return formatPrice(confirmedReservation.totalPrice());
     }
 
     /**
-     * @param reservationDto 혜택 내역 출력
+     * @param confirmedReservation
+     * @return 증정 메뉴 문자열 출력
      */
-    private void priintEventBenefits(ConfirmedReservation reservationDto) {
-        printHeader(ViewConstants.DISCOUNT_MESSAGE);
-        List<EventDto> events = reservationDto.events();
+    private String formatPromotionalGift(ConfirmedReservation confirmedReservation) {
+        GiftDto gift = confirmedReservation.gift();
+        int quantity = gift.quantity();
 
-        List<EventDto> nonZeroPriceEvents = events.stream()
-                .filter(event -> event.price() != 0)
-                .toList();
+        if (quantity != ViewConstants.ZERO) {
+            return gift.giftName() + ViewConstants.SPACE + quantity + ViewConstants.COUNT;
+        }
+        return gift.giftName();
+    }
 
-        if (nonZeroPriceEvents.isEmpty()) {
-            System.out.println(ViewConstants.NONE);
+    /**
+     * @param confirmedReservation
+     * @return 혜택 내역
+     */
+    private String formatDiscountEvents(ConfirmedReservation confirmedReservation) {
+        List<EventDto> discountEvents = confirmedReservation.events();
+
+        if (areDiscountsZero(discountEvents)) {
+            return ViewConstants.NONE;
         }
 
-        //nonZeroPriceEvents.forEach(event -> printPrice(event.price()));
-        nonZeroPriceEvents.forEach(
-                event -> System.out.println(event.eventName() + ": -" + event.price() + ViewConstants.WON));
-        printFooter();
+        return discountEvents.stream()
+                .filter(event -> event.price() != ViewConstants.ZERO)
+                .map(this::formatSingleDiscountEvent)
+                .collect(Collectors.joining(ViewConstants.NEW_LINE));
     }
 
     /**
-     * @param reservationDto 증정 메뉴 출력
+     * @param discountEvents
+     * @return 혜택 내역 목록이 0원이면 true, 아니면 false
      */
-    private void printGiftMenu(ConfirmedReservation reservationDto) {
-        printHeader(ViewConstants.GIFT_MESSAGE);
-        GiftDto gift = reservationDto.gift();
-        if (gift.quantity() != 0) {
-            System.out.println(gift.giftName() + " " + gift.quantity() + ViewConstants.COUNT);
+    private boolean areDiscountsZero(List<EventDto> discountEvents) {
+        return discountEvents.stream()
+                .allMatch(event -> event.price() == ViewConstants.ZERO);
+    }
+
+    /**
+     * @param discountEvent
+     * @return 혜택 내역 문자열
+     */
+    private String formatSingleDiscountEvent(EventDto discountEvent) {
+        return String.format(ViewConstants.LABEL_WITH_NEGATIVE_VALUE_FORMAT,
+                discountEvent.eventName(),
+                formatPrice(discountEvent.price())
+        );
+    }
+
+    /**
+     * @param confirmedReservation
+     * @return 총혜택 금액
+     */
+    private String formatTotalDiscountPrice(ConfirmedReservation confirmedReservation) {
+        int totalDiscountPrice = confirmedReservation.totalDiscountPrice();
+
+        if (totalDiscountPrice == ViewConstants.ZERO) {
+            return ViewConstants.ZERO_WON;
         }
 
-        if (gift.quantity() == 0) {
-            System.out.println(gift.giftName());
-        }
-        printFooter();
+        return String.format(ViewConstants.NEGATIVE_VALUE_FORMA, formatPrice(totalDiscountPrice));
     }
 
     /**
-     * @param reservationDto 할인 전 총주문 금액 출력
+     * @param confirmedReservation
+     * @return 할인 후 예상 결제 금액 출력 문자열
      */
-    private void printTotalReservationPrice(ConfirmedReservation reservationDto) {
-        printHeader(ViewConstants.TOTAL_RESERVATION_PRICE_MESSAGE);
-        int totalPrice = reservationDto.totalPrice();
-        printPrice(totalPrice);
-        printFooter();
+    private String formatFinalPrice(ConfirmedReservation confirmedReservation) {
+        return formatPrice(confirmedReservation.finalPrice());
     }
 
     /**
-     * @param reservationDto 주문 메뉴 출력
+     * @param confirmedReservation
+     * @return 12월 이벤트 배지 출력 문자열
      */
-    private void printReservationMenu(ConfirmedReservation reservationDto) {
-        printHeader(ViewConstants.RESERVATION_MENU_MESSAGE);
-        List<MenuItemDto> menuItemDtos = reservationDto.menuItems();
-        for (MenuItemDto menuItemDto : menuItemDtos) {
-            System.out.println(menuItemDto.menuName() + " " + menuItemDto.quantity() + ViewConstants.COUNT);
-        }
-        printFooter();
+    private String formatBadge(ConfirmedReservation confirmedReservation) {
+        BadgeDto badge = confirmedReservation.badge();
+        return badge.badgeName();
     }
 
     /**
-     * @param reservationDto 날짜 출력
-     */
-    private void printEventPreView(ConfirmedReservation reservationDto) {
-        System.out.printf(String.format(ViewConstants.START_MESSAGE + "\n", reservationDto.visitDate()));
-        printFooter();
-    }
-
-    /**
-     * @param message 출력 메시지
-     *                <p>
-     *                첫 번째 메시지를 보여준다
-     */
-    private void printHeader(String message) {
-        System.out.printf("<%s>%n", message);
-    }
-
-    /**
-     * 개행
-     */
-    private void printFooter() {
-        System.out.println();
-    }
-
-    /**
-     * @param price 금액 출력
-     */
-    private void printPrice(int price) {
-        if (price != 0) {
-            System.out.println(price + ViewConstants.WON);
-        }
-
-        if (price == 0) {
-            System.out.println(price + ViewConstants.WON);
-        }
-    }
-
-    /**
-     * 세자리 숫자 쉼표로 구분하여 출력
+     * @param price 금액
+     * @return 세 자리 숫자를 쉼표로 구분한 문자열
      */
     private String formatPrice(int price) {
-        DecimalFormat decimalFormat = new DecimalFormat("#,###원");
-        return decimalFormat.format(price);
+        return new DecimalFormat("#,###").format(price) + ViewConstants.WON;
     }
 
 }

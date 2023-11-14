8b421ccb545183ededaa0a05d4b8e384c8fabd3e
minisyu <iswater.kim@gmail.com>
2023-11-14 14:18:56 +0900

    refactor: `DiscountStorage` 수정
    - EnumMap key를 DiscountPolicy로 변경
    - addDiscountPrice 접근 제어자 private -> public으로 변경
    - 할인 혜택 메소드 삭제

--- src/main/java/christmas/domain/discount/DiscountStorage.java
+++ src/main/java/christmas/domain/discount/DiscountStorage.java
@@ -1,8 +1,5 @@
 package christmas.domain.discount;
 
-import christmas.domain.Category;
-import christmas.domain.MenuItem;
-import christmas.domain.date.VisitDate;
 import christmas.domain.dto.EventDto;
 import java.util.ArrayList;
 import java.util.EnumMap;
@@ -12,8 +9,7 @@ import java.util.List;
  * 할인 혜택 금액을 저장한다
  */
 public class DiscountStorage {
-    public final static int SPECIAL_DISCOUNT_PRICE = 1_000;
-    private final EnumMap<DiscountType, Integer> discountStorage;
+    private final EnumMap<DiscountPolicy, Integer> discountStorage;
 
     public DiscountStorage() {
         discountStorage = initializeDiscountStorage();
@@ -22,67 +18,22 @@ public class DiscountStorage {
     /**
      * @return key가 DiscountType, value를 0으로 초기화된 EnumMap
      */
-    private EnumMap<DiscountType, Integer> initializeDiscountStorage() {
-        EnumMap<DiscountType, Integer> discountStorage = new EnumMap<>(DiscountType.class);
-        for (DiscountType discountType : DiscountType.values()) {
-            discountStorage.put(discountType, 0);
+    private EnumMap<DiscountPolicy, Integer> initializeDiscountStorage() {
+        EnumMap<DiscountPolicy, Integer> discountStorage = new EnumMap<>(DiscountPolicy.class);
+        for (DiscountPolicy discountPolicy : DiscountPolicy.values()) {
+            discountStorage.put(discountPolicy, 0);
         }
         return discountStorage;
     }
 
     /**
-     * @param type  할인 혜택
-     * @param price 할인 금액
-     *              <p>
-     *              EnumMap에 할인 혜택이 적용되면 할인 금액을 저장한다
+     * @param discountPolicy 할인 혜택
+     * @param price          할인 금액
+     *                       <p>
+     *                       EnumMap에 할인 혜택이 적용되면 할인 금액을 저장한다
      */
-    private void addDiscountPrice(DiscountType type, int price) {
-        discountStorage.put(type, discountStorage.get(type) + price);
-    }
-
-    /**
-     * @param visitDate 사용자가 입력한 날짜
-     *                  <p>
-     *                  12/1 ~ 12/25 크리스마스 디데이 할인 할인
-     *                  <p>
-     *                  금액은 1000원으로 시작하여 하루마다 100원씩 증가
-     */
-    public void addChristmasDiscountPrice(VisitDate visitDate) {
-        int christmasDiscount = visitDate.calculateChristmasDiscount();
-        addDiscountPrice(DiscountType.CHRISTMAS_D_DAY, christmasDiscount);
-    }
-
-    /**
-     * @param menuItem 사용자가 입력한 메뉴와 개수
-     *                 <p>
-     *                 평일 할인(일요일~목요일): 평일에는 디저트 메뉴를 메뉴 1개당 2,023원 할인
-     */
-    public void addWeekDaysDiscountPrice(MenuItem menuItem) {
-        if (menuItem.isSameCategory(Category.DESSERT)) {
-            addDiscountPrice(DiscountType.WEEKDAYS, menuItem.calculateWeekDiscountPrice());
-        }
-    }
-
-    /**
-     * @param menuItem 사용자가 입력한 메뉴와 개수
-     *                 <p>
-     *                 주말 할인(금요일, 토요일): 주말에는 메인 메뉴를 메뉴 1개당 2,023원 할인
-     */
-    public void addWeekendDiscountPrice(MenuItem menuItem) {
-        if (menuItem.isSameCategory(Category.MAINDISH)) {
-            addDiscountPrice(DiscountType.WEEKEND, menuItem.calculateWeekDiscountPrice());
-        }
-    }
-
-    /**
-     * @param visitDate 사용자가 입력한 날짜
-     *                  <p>
-     *                  이벤트 달력에 별(⭐)이 있으면 총 주문 금액에서 1,000원 할인
-     */
-    public void addSpecialDisCountPrice(VisitDate visitDate) {
-        if (visitDate.isSpecialDay()) {
-            addDiscountPrice(DiscountType.SPECIAL, SPECIAL_DISCOUNT_PRICE);
-        }
+    public void addDiscountPrice(DiscountPolicy discountPolicy, int price) {
+        discountStorage.put(discountPolicy, discountStorage.get(discountPolicy) + price);
     }
 
     /**

16e25bd0575968df944d9fb6353f32e7a5b85945
minisyu <iswater.kim@gmail.com>
2023-11-15 00:22:25 +0900

    refactor: `InputView` 수정
    - ViewConstants의 상수 사용
    - 메소드 위치 이동

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -4,43 +4,40 @@ import camp.nextstep.edu.missionutils.Console;
 import christmas.domain.reservation.Reservation;
 import christmas.domain.reservation.date.VisitDate;
 import christmas.domain.reservation.menu.MenuItems;
+import christmas.util.ViewConstants;
 import christmas.validator.InputValidator;
 import java.util.function.Supplier;
 
 public class InputView {
-    private static final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
-    private static final String VISIT_DATE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
-    private static final String MENU_ITEMS_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
-
-    private <T> T retryIfThrow(Supplier<T> supplier) {
-        while (true) {
-            try {
-                return supplier.get();
-            } catch (IllegalArgumentException e) {
-                System.out.printf("[ERROR] %s\n", e.getMessage());
-            }
-        }
-    }
-
     public Reservation inputReservation() {
-        System.out.println(START_MESSAGE);
+        System.out.println(ViewConstants.START_EVENT_PLANNER_MESSAGE);
         VisitDate visitDate = retryIfThrow(this::inputVisitDate);
         MenuItems menuItems = retryIfThrow(this::inputMenuItems);
         return Reservation.from(visitDate, menuItems);
     }
 
     public MenuItems inputMenuItems() {
-        System.out.println(MENU_ITEMS_MESSAGE);
+        System.out.println(ViewConstants.MENU_ITEMS_MESSAGE);
         String input = readLine();
         InputValidator.validateMenuItemsInputFormat(input);
         return MenuItems.from(input);
     }
 
     public VisitDate inputVisitDate() {
-        System.out.println(VISIT_DATE_MESSAGE);
+        System.out.println(ViewConstants.VISIT_DATE_MESSAGE);
         String input = readLine();
         InputValidator.validateVisitDateString(input);
         return VisitDate.from(input);
+    }
+
+    private <T> T retryIfThrow(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(ViewConstants.ERROR_PREFIX + e.getMessage());
+            }
+        }
     }
 
     private String readLine() {

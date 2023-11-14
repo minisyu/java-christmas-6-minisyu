5e0d1ba66924691475375f4c10cf24a08a27f22f
minisyu <iswater.kim@gmail.com>
2023-11-15 00:20:42 +0900

    refactor: `ViewConstants` 수정
    - InputView에서 사용하는 상수 이동
    - ERROR PREFIX 추가

--- src/main/java/christmas/util/ViewConstants.java
+++ src/main/java/christmas/util/ViewConstants.java
@@ -1,6 +1,11 @@
 package christmas.util;
 
 public class ViewConstants {
+    public static final String START_EVENT_PLANNER_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    public static final String VISIT_DATE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    public static final String MENU_ITEMS_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    public static final String ERROR_PREFIX = "[ERROR] ";
+
     public static final String SPACE = " ";
     public static final String NEW_LINE = "\n";
 

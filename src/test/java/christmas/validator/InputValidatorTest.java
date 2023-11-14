91f150e7fdb57b8d53b2f2c9a7956f085a4043d0
minisyu <iswater.kim@gmail.com>
2023-11-14 21:55:32 +0900

    refactor: `InputValidatorTest` 수정

--- src/test/java/christmas/validator/InputValidatorTest.java
+++ src/test/java/christmas/validator/InputValidatorTest.java
@@ -1,7 +1,7 @@
 package christmas.validator;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import christmas.exception.MenuInputException;
 import org.junit.jupiter.api.DisplayName;
@@ -14,23 +14,32 @@ class InputValidatorTest {
     @DisplayName("메뉴와 개수 입력 형식 유효성 검사 - 성공")
     @Test
     public void should_Validate_MenuItems_Input_Format1() {
+        // given
         String input = "아이스크림-2";
-        InputValidator.validateMenuItemsInputFormat(input);
-        assertTrue(true);
+
+        // when
+        // then
+        assertDoesNotThrow(() -> InputValidator.validateMenuItemsInputFormat(input));
+
     }
 
     @DisplayName("메뉴와 개수 입력 형식 유효성 검사 - 성공")
     @Test
     public void should_Validate_MenuItems_Input_Format2() {
+        // given
         String input = "아이스크림-2,티본스테이크-1";
-        InputValidator.validateMenuItemsInputFormat(input);
-        assertTrue(true);
+
+        // when
+        // then
+        assertDoesNotThrow(() -> InputValidator.validateMenuItemsInputFormat(input));
     }
 
     @DisplayName("메뉴와 개수 입력 형식 유효성 검사 - 실패")
     @ParameterizedTest
     @ValueSource(strings = {"아이스크림-2,티본스테이크", "아이스크림-2,티본스테이크-1,", ",아이스크림-2"})
     public void should_Throw_Exception_For_Invalid_MenuItems_Input_Format(String input) {
+        // when
+        // then
         assertThatThrownBy(() -> InputValidator.validateMenuItemsInputFormat(input))
                 .isInstanceOf(MenuInputException.class);
     }

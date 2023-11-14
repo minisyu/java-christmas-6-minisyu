19926b6e79d8153683b9c5f849d6838ef001a1d3
minisyu <iswater.kim@gmail.com>
2023-11-14 14:52:54 +0900

    refactor: `DiscountStorageTest` 수정
    - 할인 혜택 금액이 저장되는지 확인
    - 할인 혜택의 총 금액을 올바르게 계산하는지 확인
    - EventDto를 생성하는지 확인

--- src/test/java/christmas/domain/discount/DiscountStorageTest.java
+++ src/test/java/christmas/domain/discount/DiscountStorageTest.java
@@ -1,16 +1,14 @@
 package christmas.domain.discount;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 
 import christmas.domain.MenuItem;
-import christmas.domain.date.VisitDate;
 import christmas.domain.dto.EventDto;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class DiscountStorageTest {
     private DiscountStorage discountStorage;
@@ -20,84 +18,57 @@ class DiscountStorageTest {
         discountStorage = new DiscountStorage();
     }
 
-    @DisplayName("크리스마스 디데이 할인(12/1 ~ 12/25) - 1,000원으로 시작하여 크리스마스가 다가올수록 날마다 할인 금액이 100원씩 증가")
-    @ParameterizedTest
-    @ValueSource(strings = {"1", "8", "17", "24", "25"})
-    void should_Add_Christmas_Discount_Price(String input) {
-        // given
-        VisitDate visitDate = VisitDate.from(input);
-
-        // when
-        discountStorage.addChristmasDiscountPrice(visitDate);
-
-        // then
-        assertEquals(visitDate.calculateChristmasDiscount(),
-                discountStorage.calculateTotalDiscountPrice());
-    }
-
-    @DisplayName("평일 할인(일요일~목요일) - 평일에는 디저트 메뉴를 메뉴 1개당 2,023원 할인")
+    @DisplayName("할인 혜택 금액이 저장되는지 확인")
     @Test
-    void should_Add_WeekDays_Discount_Price() {
+    void should_Store_Correct_Discount_Price() {
         // given
-        String input = "아이스크림-4";
-        MenuItem menuItem = MenuItem.from(input);
+        discountStorage.addDiscountPrice(DiscountPolicy.WEEKDAYS, MenuItem.WEEK_DISCOUNT_PRICE);
+        discountStorage.addDiscountPrice(DiscountPolicy.SPECIAL, DiscountPolicy.SPECIAL_DISCOUNT_PRICE);
 
         // when
-        discountStorage.addWeekDaysDiscountPrice(menuItem);
+        int totalDiscountPrice = discountStorage.calculateTotalDiscountPrice();
+        int storedTotalPrice = MenuItem.WEEK_DISCOUNT_PRICE + DiscountPolicy.SPECIAL_DISCOUNT_PRICE;
 
         // then
-        assertEquals(MenuItem.WEEK_DISCOUNT_PRICE * 4, discountStorage.calculateTotalDiscountPrice());
+        assertThat(totalDiscountPrice).isEqualTo(storedTotalPrice);
     }
 
-    @DisplayName("주말 할인(금요일, 토요일) - 주말에는 메인 메뉴를 메뉴 1개당 2,023원 할인")
+    @DisplayName("할인 혜택의 총 금액을 올바르게 계산하는지 확인")
     @Test
-    void should_Add_Weekend_Discount_Price() {
+    void should_Calculate_Total_Discount_Price() {
         // given
-        String input = "티본스테이크-2";
-        MenuItem menuItem = MenuItem.from(input);
+        discountStorage.addDiscountPrice(DiscountPolicy.WEEKDAYS, MenuItem.WEEK_DISCOUNT_PRICE * 2);
+        discountStorage.addDiscountPrice(DiscountPolicy.WEEKEND, MenuItem.WEEK_DISCOUNT_PRICE);
+        discountStorage.addDiscountPrice(DiscountPolicy.SPECIAL, DiscountPolicy.SPECIAL_DISCOUNT_PRICE * 3);
 
         // when
-        discountStorage.addWeekendDiscountPrice(menuItem);
+        int totalDiscountPrice = discountStorage.calculateTotalDiscountPrice();
 
         // then
-        assertEquals(MenuItem.WEEK_DISCOUNT_PRICE * 2, discountStorage.calculateTotalDiscountPrice());
+        assertThat(totalDiscountPrice).isEqualTo(9_069);
     }
 
-    @DisplayName("특별 할인 - 이벤트 달력에 별이 있으면 총주문 금액에서 1,000원 할인")
-    @ParameterizedTest
-    @ValueSource(strings = {"3", "10", "17", "24", "31"})
-    void should_Add_Special_DisCount_Price(String input) {
-        // given
-        VisitDate visitDate = VisitDate.from(input);
 
-        // when
-        discountStorage.addSpecialDisCountPrice(visitDate);
-
-        // then
-        assertEquals(DiscountStorage.SPECIAL_DISCOUNT_PRICE, discountStorage.calculateTotalDiscountPrice());
-    }
-
-    @DisplayName("DiscountStorage에 저장된 데이터로 List<EventDto> 변환")
+    @DisplayName("EventDto를 생성하는지 확인")
     @Test
-    void should_Convert_To_EventsDto() {
+    void should_Create_EventsDto() {
         // given
-        String input1 = "3";
-        VisitDate visitDate = VisitDate.from(input1);
-        discountStorage.addChristmasDiscountPrice(visitDate);
-        discountStorage.addSpecialDisCountPrice(visitDate);
-
-        String input2 = "아이스크림-1";
-        MenuItem menuItem1 = MenuItem.from(input2);
-        discountStorage.addWeekDaysDiscountPrice(menuItem1);
-
-        String input3 = "티본스테이크-2";
-        MenuItem menuItem = MenuItem.from(input3);
-        discountStorage.addWeekendDiscountPrice(menuItem);
+        discountStorage.addDiscountPrice(DiscountPolicy.CHRISTMAS_D_DAY, 1000);
+        discountStorage.addDiscountPrice(DiscountPolicy.WEEKDAYS, MenuItem.WEEK_DISCOUNT_PRICE);
+        discountStorage.addDiscountPrice(DiscountPolicy.WEEKEND, MenuItem.WEEK_DISCOUNT_PRICE);
+        discountStorage.addDiscountPrice(DiscountPolicy.SPECIAL, DiscountPolicy.SPECIAL_DISCOUNT_PRICE);
 
         // when
-        List<EventDto> eventDtos = discountStorage.toEventsDto();
+        List<EventDto> eventsDto = discountStorage.toEventsDto();
 
         // then
-        assertEquals(4, eventDtos.size());
+        assertThat(eventsDto).hasSize(4)
+                .extracting(EventDto::eventName, EventDto::price)
+                .containsExactly(
+                        tuple("크리스마스 디데이 할인", 1_000),
+                        tuple("평일 할인", MenuItem.WEEK_DISCOUNT_PRICE),
+                        tuple("주말 할인", MenuItem.WEEK_DISCOUNT_PRICE),
+                        tuple("특별 할인", DiscountPolicy.SPECIAL_DISCOUNT_PRICE)
+                );
     }
 }

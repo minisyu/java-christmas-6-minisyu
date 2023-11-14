67408eed51d159fbbc7b9aece574ba4408cab5ce
minisyu <iswater.kim@gmail.com>
2023-11-14 21:53:06 +0900

    refactor: `MenuTest` 수정

--- src/test/java/christmas/domain/MenuTest.java
+++ src/test/java/christmas/domain/MenuTest.java
@@ -1,7 +1,7 @@
 package christmas.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import christmas.exception.MenuInputException;
 import org.junit.jupiter.api.DisplayName;
@@ -15,20 +15,27 @@ class MenuTest {
     @Test
     public void should_Return_Enum_Constant_For_MenuName() {
         // given
-        String menuName = "시저샐러드";
+        String menuName1 = "시저샐러드";
+        String menuName2 = "해산물파스타";
+        String menuName3 = "아이스크림";
+        String menuName4 = "레드와인";
 
         // when
-        Menu menu = Menu.from(menuName);
+        Menu menu1 = Menu.from(menuName1);
+        Menu menu2 = Menu.from(menuName2);
+        Menu menu3 = Menu.from(menuName3);
+        Menu menu4 = Menu.from(menuName4);
 
         // then
-        assertEquals(menuName, menu.getFoodName());
-        assertEquals(menu.getFoodPrice(), 8_000);
-        assertEquals(menu.getCategory(), Category.APPETIZER);
+        assertThat(menu1.getCategory()).isEqualTo(Category.APPETIZER);
+        assertThat(menu2.getCategory()).isEqualTo(Category.MAINDISH);
+        assertThat(menu3.getCategory()).isEqualTo(Category.DESSERT);
+        assertThat(menu4.getCategory()).isEqualTo(Category.DRINK);
     }
 
     @DisplayName("from 메소드 - 존재하지 않는 메뉴명으로 예외 발생")
     @ParameterizedTest
-    @ValueSource(strings = {"짜장면", "짬뽕", "김밥", "아메리카노", "콘샐러드"})
+    @ValueSource(strings = {"짜장면", "짬뽕", "김밥", "아메리카노", "콘샐러드", "바닐라라떼", "딸기생크림케이크"})
     public void should_Throw_Exception_For_Non_existent_MenuName(String menuName) {
         // when
         // then

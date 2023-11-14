df3a8e62ace694013e44629ee2de98e578d5a056
minisyu <iswater.kim@gmail.com>
2023-11-14 19:04:10 +0900

    refactor: `ViewConstants` 수정
    - 이벤트 해택 내역 템플릿 상수 추가
    - 출력 메시지 상수 제거

--- src/main/java/christmas/util/ViewConstants.java
+++ src/main/java/christmas/util/ViewConstants.java
@@ -1,18 +1,41 @@
 package christmas.util;
 
 public class ViewConstants {
-    public static final String START_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
-    public static final String RESERVATION_MENU_MESSAGE = "주문 메뉴";
-    public static final String TOTAL_RESERVATION_PRICE_MESSAGE = "할인 전 총주문 금액";
-    public static final String GIFT_MESSAGE = "증정 메뉴";
-    public static final String DISCOUNT_MESSAGE = "혜택 내역";
-    public static final String TOTAL_DISCOUNT_MESSAGE = "총혜택 금액";
-    public static final String EXPECTED_PAYMENT_MESSAGE = "할인 후 예상 결제 금액";
-    public static final String BADGE_MESSAGE = "12월 이벤트 배지";
+    public static final String SPACE = " ";
+    public static final String NEW_LINE = "\n";
 
-    public static final String MINUS = "-";
+    public static final int ZERO = 0;
     public static final String WON = "원";
+    public static final String ZERO_WON = ZERO + WON;
+
     public static final String COUNT = "개";
     public static final String NONE = "없음";
 
+    public static final String NEGATIVE_VALUE_FORMA = "-%s";
+    public static final String LABEL_WITH_NEGATIVE_VALUE_FORMAT = "%s: -%s";
+
+    public static final String RESERVATION_MESSAGE_FRAME = """
+            12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 s보기!
+                        
+            <주문 메뉴>
+            %s
+                        
+            <할인 전 총주문 금액>
+            %s
+                        
+            <증정 메뉴>
+            %s
+                        
+            <혜택 내역>
+            %s
+                        
+            <총혜택 금액>
+            %s
+                        
+            <할인 후 예상 결제 금액>
+            %s
+                        
+            <12월 이벤트 배지>
+            %s
+            """;
 }
